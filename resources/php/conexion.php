11cb6186a2dd0c8395d9e70dd6b0aace8c7d8d0d
FranRob <108472146+FranRob@users.noreply.github.com>
2023-09-15 19:00:06 -0300

    conexion

---
+++ resources/php/conexion.php
@@ -0,0 +1,12 @@
+<?php
+
+$host = "localHost";
+$dbname = "sistema_asistencias";
+$userName = "root";
+$password = "";
+
+
+$conection = new PDO('mysql:host=localhost;dbname=asistencias', $userName, $password);
+
+
+?>
