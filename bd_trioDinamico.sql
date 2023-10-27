a453fc03ae1da7ba0c0ef657ee404d5bb8bd8496
Franco Robles <108472146+FranRob@users.noreply.github.com>
2023-10-27 11:29:22 -0300

    nueva bd

--- bd_trioDinamico.sql
+++ bd_trioDinamico.sql
@@ -22,40 +22,13 @@ USE `sistema_asistencias`;
 -- Volcando estructura para tabla sistema_asistencias.alumno
 CREATE TABLE IF NOT EXISTS `alumno` (
   `dni` int NOT NULL,
-  `nombre` varchar(30) CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci NOT NULL,
+  `nombre` varchar(30) NOT NULL,
   `apellido` varchar(30) NOT NULL,
   `fecha_nacimiento` date NOT NULL,
   PRIMARY KEY (`dni`) USING BTREE
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
 
--- Volcando datos para la tabla sistema_asistencias.alumno: ~4 rows (aproximadamente)
-INSERT INTO `alumno` (`dni`, `nombre`, `apellido`, `fecha_nacimiento`) VALUES
-	(38570361, 'Marcos', 'Reynoso', '2023-10-07'),
-	(39255959, 'Franco Antonio', 'Robles', '2023-10-07'),
-	(40018598, 'Kevin Gustavo', 'Quiroga', '2023-10-07'),
-	(40790201, 'Esteban ', 'Copello', '2023-10-07'),
-	(40790545, 'Daian Exequiel', 'Fernandez', '2023-10-07'),
-	(41872676, 'Facundo Ariel', 'Janusa', '2023-10-07'),
-	(42069298, 'Marcos Damián', 'Godoy', '2023-10-07'),
-	(42070085, 'Maria Pia', 'Melgarejo', '2023-10-07'),
-	(42850626, 'Lucas', 'Barreiro', '2023-10-07'),
-	(43149316, 'Franco Agustin', 'Chappe', '2023-10-07'),
-	(43414566, 'Maximiliano', 'Weyler', '2023-10-07'),
-	(43631710, 'Thiago Jeremías', 'Meseguer', '2023-10-07'),
-	(43631803, 'Bruno', 'Godoy', '2023-10-07'),
-	(43632750, 'Roman', 'Coletti', '2023-10-07'),
-	(44282007, 'Bianca Ariana', 'Quiroga', '2023-10-07'),
-	(44623314, 'Facundo Gerónimo', 'Figún', '2023-10-07'),
-	(44644523, 'Ignacio Agustin', 'Piter', '2023-10-07'),
-	(44980999, 'Nicolas Osvaldo', 'Fernandez', '2023-10-07'),
-	(44981059, 'Federico José', 'Martinolich', '2023-10-07'),
-	(45048325, 'Felipe', 'Franco', '2023-10-07'),
-	(45048950, 'Facundo Martin ', 'Jara', '2023-10-07'),
-	(45385675, 'Teo', 'Hildt', '2023-10-07'),
-	(45387761, 'Santiago Nicolas', 'Martinez Bender', '2023-10-07'),
-	(45389325, 'Lucas Jeremias', 'Fiorotto', '2023-10-07'),
-	(45741185, 'Pablo Federico', 'Martinez', '2023-10-07'),
-	(45847922, 'Franco', 'Cabrera', '2023-10-07');
+-- La exportación de datos fue deseleccionada.
 
 -- Volcando estructura para tabla sistema_asistencias.asistencias
 CREATE TABLE IF NOT EXISTS `asistencias` (
@@ -65,16 +38,18 @@ CREATE TABLE IF NOT EXISTS `asistencias` (
   PRIMARY KEY (`id_asistencia`) USING BTREE,
   KEY `DNI_alumno` (`dni_alumno`) USING BTREE,
   CONSTRAINT `asistencias_ibfk_1` FOREIGN KEY (`dni_alumno`) REFERENCES `alumno` (`dni`)
-) ENGINE=InnoDB AUTO_INCREMENT=9 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+) ENGINE=InnoDB AUTO_INCREMENT=75 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
 
--- Volcando datos para la tabla sistema_asistencias.asistencias: ~2 rows (aproximadamente)
+-- La exportación de datos fue deseleccionada.
 
 -- Volcando estructura para tabla sistema_asistencias.parametros
 CREATE TABLE IF NOT EXISTS `parametros` (
-  `dias_de_clases` int DEFAULT NULL
+  `dias_de_clases` int DEFAULT NULL,
+  `porcentaje_promocion` int DEFAULT NULL,
+  `porcentaje_regular` int DEFAULT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
 
--- Volcando datos para la tabla sistema_asistencias.parametros: ~0 rows (aproximadamente)
+-- La exportación de datos fue deseleccionada.
 
 -- Volcando estructura para tabla sistema_asistencias.profesores
 CREATE TABLE IF NOT EXISTS `profesores` (
@@ -84,7 +59,7 @@ CREATE TABLE IF NOT EXISTS `profesores` (
   PRIMARY KEY (`dni_profesor`) USING BTREE
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
 
--- Volcando datos para la tabla sistema_asistencias.profesores: ~0 rows (aproximadamente)
+-- La exportación de datos fue deseleccionada.
 
 /*!40103 SET TIME_ZONE=IFNULL(@OLD_TIME_ZONE, 'system') */;
 /*!40101 SET SQL_MODE=IFNULL(@OLD_SQL_MODE, '') */;
