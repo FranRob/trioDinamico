527e389308150d8bc76574a4170eaaaa07a5c378
Franco Robles <108472146+FranRob@users.noreply.github.com>
2023-11-13 23:26:50 -0300

    BD- Last Version

--- bd_trioDinamico.sql
+++ bd_trioDinamico.sql
@@ -31,7 +31,7 @@ CREATE TABLE IF NOT EXISTS `alumno` (
 -- Volcando datos para la tabla sistema_asistencias.alumno: ~26 rows (aproximadamente)
 INSERT INTO `alumno` (`dni`, `nombre`, `apellido`, `fecha_nacimiento`) VALUES
 	(38570361, 'Marcos', 'Reynoso', '2023-10-07'),
-	(39255959, 'Franco Antonio', 'Robles', '2023-10-07'),
+	(39255959, 'Franco Antonio', 'Robles', '1995-10-12'),
 	(40018598, 'Kevin Gustavo', 'Quiroga', '2023-10-07'),
 	(40790201, 'Esteban ', 'Copello', '2023-10-07'),
 	(40790545, 'Daian Exequiel', 'Fernandez', '2023-10-07'),
@@ -65,42 +65,9 @@ CREATE TABLE IF NOT EXISTS `asistencias` (
   PRIMARY KEY (`id_asistencia`) USING BTREE,
   KEY `DNI_alumno` (`dni_alumno`) USING BTREE,
   CONSTRAINT `asistencias_ibfk_1` FOREIGN KEY (`dni_alumno`) REFERENCES `alumno` (`dni`)
-) ENGINE=InnoDB AUTO_INCREMENT=75 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+) ENGINE=InnoDB AUTO_INCREMENT=297 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
 
--- Volcando datos para la tabla sistema_asistencias.asistencias: ~32 rows (aproximadamente)
-INSERT INTO `asistencias` (`id_asistencia`, `dni_alumno`, `fecha`) VALUES
-	(43, 42070085, '2023-10-20 03:00:00'),
-	(44, 44282007, '2023-10-20 03:00:00'),
-	(45, 39255959, '2023-10-20 03:00:00'),
-	(46, 42850626, '2023-10-20 03:00:00'),
-	(47, 44623314, '2023-10-20 03:00:00'),
-	(48, 45048950, '2023-10-20 03:00:00'),
-	(49, 42850626, '2023-10-20 03:00:00'),
-	(50, 45847922, '2023-10-10 03:00:00'),
-	(51, 43149316, '2023-10-20 03:00:00'),
-	(52, 43632750, '2023-10-10 03:00:00'),
-	(53, 40790201, '2023-10-20 03:00:00'),
-	(54, 40790545, '2023-10-20 03:00:00'),
-	(55, 44980999, '2023-10-20 03:00:00'),
-	(56, 44623314, '2023-10-20 03:00:00'),
-	(57, 45389325, '2023-10-20 03:00:00'),
-	(58, 45048325, '2023-10-20 03:00:00'),
-	(59, 42069298, '2023-10-20 03:00:00'),
-	(60, 43631803, '2023-10-20 03:00:00'),
-	(61, 45385675, '2023-10-20 03:00:00'),
-	(62, 41872676, '2023-10-20 03:00:00'),
-	(63, 45048950, '2023-10-20 03:00:00'),
-	(64, 45741185, '2023-10-20 03:00:00'),
-	(65, 45387761, '2023-10-20 03:00:00'),
-	(66, 44981059, '2023-10-20 03:00:00'),
-	(67, 42070085, '2023-10-20 03:00:00'),
-	(68, 43631710, '2023-10-20 03:00:00'),
-	(69, 44644523, '2023-10-20 03:00:00'),
-	(70, 40018598, '2023-10-20 03:00:00'),
-	(71, 44282007, '2023-10-10 03:00:00'),
-	(72, 38570361, '2023-10-20 03:00:00'),
-	(73, 39255959, '2023-10-20 03:00:00'),
-	(74, 43414566, '2023-10-20 03:00:00');
+-- Volcando datos para la tabla sistema_asistencias.asistencias: ~0 rows (aproximadamente)
 
 -- Volcando estructura para tabla sistema_asistencias.parametros
 CREATE TABLE IF NOT EXISTS `parametros` (
@@ -109,17 +76,9 @@ CREATE TABLE IF NOT EXISTS `parametros` (
   `porcentaje_regular` int DEFAULT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
 
--- Volcando datos para la tabla sistema_asistencias.parametros: ~0 rows (aproximadamente)
-
--- Volcando estructura para tabla sistema_asistencias.profesores
-CREATE TABLE IF NOT EXISTS `profesores` (
-  `nombre` varchar(50) DEFAULT NULL,
-  `apellido` varchar(50) DEFAULT NULL,
-  `dni_profesor` int NOT NULL,
-  PRIMARY KEY (`dni_profesor`) USING BTREE
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
-
--- Volcando datos para la tabla sistema_asistencias.profesores: ~0 rows (aproximadamente)
+-- Volcando datos para la tabla sistema_asistencias.parametros: ~1 rows (aproximadamente)
+INSERT INTO `parametros` (`dias_de_clases`, `porcentaje_promocion`, `porcentaje_regular`) VALUES
+	(80, 80, 60);
 
 /*!40103 SET TIME_ZONE=IFNULL(@OLD_TIME_ZONE, 'system') */;
 /*!40101 SET SQL_MODE=IFNULL(@OLD_SQL_MODE, '') */;
